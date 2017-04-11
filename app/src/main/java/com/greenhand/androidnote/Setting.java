253ca89fd6ea7d281d3184f98c60ed829fe39089
houshengli <1414006272@qq.com>
2017-04-11 21:20:39 +0800

    增加设置功能

---
+++ app/src/main/java/com/greenhand/androidnote/Setting.java
@@ -0,0 +1,17 @@
+package com.greenhand.androidnote;
+
+/**
+ * project: AndroidNote
+ * package: com.greenhand.androidnote
+ * author: HouShengLi
+ * time: 2017/4/11 21:19
+ * e-mail:13967189624@163.com
+ * description:
+ */
+
+public class Setting {
+
+    /**
+     * 各种设置
+     */
+}
